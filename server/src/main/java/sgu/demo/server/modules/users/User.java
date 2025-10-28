ff1919e340aba9db83e24bc0331695a3fd0da38f
“ChrisAle117” <20223tn105@utez.edu.mx>
2025-10-27 18:30:49 -0600

    commit try 5

--- server/src/main/java/sgu/demo/server/modules/users/User.java
+++ server/src/main/java/sgu/demo/server/modules/users/User.java
@@ -18,7 +18,7 @@ public class User {
     @Column(nullable = false)
     private String phone;
 
-    // Getters y Setters
+
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
